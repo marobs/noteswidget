ced2b6ffaee2e3a53655df2abee754744d00f311
Michael Roberts <mroberts@fitbit.com>
2020-07-01 11:11:07 -0700

    Initial Commit

---
+++ noteswidget/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  noteswidget
+//
+//  Created by Michael Roberts on 7/1/20.
+//  Copyright © 2020 Michael Roberts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ noteswidget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  noteswidget
+//
+//  Created by Michael Roberts on 7/1/20.
+//  Copyright © 2020 Michael Roberts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
