6125888337b271d02ced05d5ded3a717a6633157
Michael Roberts <mroberts@fitbit.com>
2020-07-02 11:03:39 -0700

    Basic text box

--- noteswidget/ViewController.swift
+++ noteswidget/ViewController.swift
@@ -8,10 +8,18 @@
 
 import Cocoa
 
+// Manages my view with the text field inside
 class ViewController: NSViewController {
 
+    @IBOutlet var bigOlTextField: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // did load, will appear, did appear, will disappear, etc.
+        
+        // Use file manager to load data
+        // Make the view control the delegate for the text field
+        //    A design patter on iOS wher you delegate some behavior
 
         // Do any additional setup after loading the view.
     }
@@ -25,3 +33,14 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSTextViewDelegate {
+    
+    
+    
+    // Delegating some of the text view behavior to this class
+    // Textview will call this class with callbacks
+    // We want to hook up the "something changed in this UI Object" callback in here
+    func textDidChange(_ notification: Notification) {
+        print(bigOlTextField.string)
+    }
+}
